e5a8a4dc5acef00e7d02cf89449f252831294a22
Bohdan <leo160886@gmail.com>
2018-11-20 17:51:29 +0200

    removed_token

--- parser.py
+++ parser.py
@@ -4,8 +4,6 @@ import json
 import time
 import os
 
-
-# url = "https://api.telegram.org/bot758261552:AAE1zVA2sHNw_WxDtVZolbLivX3-W8Xhd6k/"
 bot_token = (os.getenv("TOKEN"))
 url = "https://api.telegram.org/bot" + bot_token + "/"
 
