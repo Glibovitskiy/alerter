c2d68dcb3e7d9053f1552b5e399e0bd203f6e80e
Bohdan <leo160886@gmail.com>
2018-11-21 18:23:39 +0200

    modifyed_apibot

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM alpine:3.8
 WORKDIR /OPT
 Copy . /OPT
 RUN apk add --update py-pip
-RUN pip install requests
+RUN pip install requests py-telegram-bot
 CMD [ "python", "./parser.py" ]
--- parser.py
+++ parser.py
@@ -1,14 +1,10 @@
 #! /usr/bin/python
-import requests
-import json
-import time
-import os
-import logging
-
-logging.basicConfig(filename="test.log", level=logging.DEBUG)
+import telegram, json, requests, time, os
 
 bot_token = (os.getenv("TOKEN"))
-url = "https://api.telegram.org/bot" + bot_token + "/"
+bot = telegram.Bot(token=bot_token)
+chat_id = bot.get_updates()[-1].message.chat_id
+
 mhz = None
 mhz2 = None
 def update_hasrate():
@@ -26,27 +22,8 @@ lst=update_hasrate()
 print ("ReportedHashrate_bh: " + str(mhz) + " MH/s")
 print ("ReportedHashrate_bg: " + str(mhz2) + " MH/s")
 
-def get_updates_json(request):
-    response = requests.get(request + 'getUpdates')
-    return response.json()
-
-def last_update(data):
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
-
-def get_chat_id(update):
-    chat_id = update['message']['chat']['id']
-    return chat_id
-
-def send_mess(chat, text):
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
-
-chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, "Monitoring service has been started\n" "ReportedHashrate_bh: " + str(mhz) + " MH/s\n" "ReportedHashrate_bg: " + str(mhz2) + " MH/s")
-send_mess(chat_id, u'\U0001F4B0' + u"\U0001F680" + u'\U0001F4B0' + u"\U0001F680" + u'\U0001F4B0' + u"\U0001F680")
+bot.send_message(chat_id=chat_id, text="Monitoring service has been started\n" "ReportedHashrate_bh: " + str(mhz) + " MH/s\n" "ReportedHashrate_bg: " + str(mhz2) + " MH/s")
+bot.send_message(chat_id=chat_id, text=u'\U0001F4B0' + u"\U0001F680" + u'\U0001F4B0' + u"\U0001F680" + u'\U0001F4B0' + u"\U0001F680")
 
 def monitoring():
     update_hasrate()
@@ -57,4 +34,6 @@ def monitoring():
         time.sleep(2000)
 while True:
     monitoring()
-    logging.debug(monitoring)
+#requirements
+#pip install requsts
+#758261552:AAE1zVA2sHNw_WxDtVZolbLivX3-W8Xhd6k
