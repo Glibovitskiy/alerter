404c818eecc64b6a00441fd39de2b06b4e294f84
Bohdan <leo160886@gmail.com>
2018-11-26 15:00:47 +0200

    git add .

--- parser.py
+++ parser.py
@@ -8,16 +8,17 @@ chat_id = "-244599378"
 mhz = None
 mhz2 = None
 def update_hasrate():
-    response = requests.get("https://api.ethermine.org/miner/:0x6ddd79c6e71d4bfca125f4ae38c578af8c103daa/history/")
-    data = response.json()["data"][len(response.json()["data"])-1]
-    global mhz
-    mhz = (int(data.get(u'reportedHashrate')))/1000000
-    response2 = requests.get("https://api.ethermine.org/miner/:0x93665d08f3581c1fa4cb30eaadee0b18ddc7b6cb/history/")
-    data2 = response2.json()["data"][len(response2.json()["data"])-1]
-    global mhz2
-    mhz2 = (int(data2.get(u'reportedHashrate')))/1000000
-    return mhz, mhz2
-
+    try:
+        response = requests.get("https://api.ethermine.org/miner/:0x6ddd79c6e71d4bfca125f4ae38c578af8c103daa/history/")
+        data = response.json()["data"][len(response.json()["data"])-1]
+        global mhz
+        mhz = (int(data.get(u'reportedHashrate')))/1000000
+        response2 = requests.get("https://api.ethermine.org/miner/:0x93665d08f3581c1fa4cb30eaadee0b18ddc7b6cb/history/")
+        data2 = response2.json()["data"][len(response2.json()["data"])-1]
+        global mhz2
+        mhz2 = (int(data2.get(u'reportedHashrate')))/1000000
+        return mhz, mhz2
+    except TypeError:
 lst=update_hasrate()
 print ("ReportedHashrate_bh: " + str(mhz) + " MH/s")
 print ("ReportedHashrate_bg: " + str(mhz2) + " MH/s")
@@ -27,11 +28,10 @@ bot.send_message(chat_id=chat_id, text=u'\U0001F4B0' + u"\U0001F680" + u'\U0001F
 
 def monitoring():
     update_hasrate()
-    if mhz > 100:
-        if mhz2 > 100:
-            time.sleep(600)
+    if mhz > 100 and mhz2 > 100:
+        time.sleep(600)
     else:
-        send_mess(chat_id, "Mining has been down, needs maintenance" + u"\U0001F4A3" + u"\U0001F4A3" + u"\U0001F4A3")
+        bot.send_message(chat_id, "Mining has been down, needs maintenance" + u"\U0001F4A3" + u"\U0001F4A3" + u"\U0001F4A3")
         time.sleep(2000)
 while True:
     monitoring()
