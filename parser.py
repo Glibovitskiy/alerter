5103f5d32440933ee1dd9e14cffa6669228f9ad2
Bohdan Glibovytskiy <Leo160886@gmail.com>
2019-01-01 14:52:19 +0200

    1_instance

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import telegram, json, requests, time, os
 
 bot = telegram.Bot(token=os.getenv("TOKEN"))
 chat_id = "-244599378"
-# bot.get_updates()[-1].message.chat_id 
+# bot.get_updates()[-1].message.chat_id
 
 mhz = None
 mhz2 = None
@@ -13,24 +13,25 @@ def update_hasrate():
         data = response.json()["data"][len(response.json()["data"])-1]
         global mhz
         mhz = (int(data.get(u'reportedHashrate')))/1000000
-        response2 = requests.get("https://api.ethermine.org/miner/:0x93665d08f3581c1fa4cb30eaadee0b18ddc7b6cb/history/")
-        data2 = response2.json()["data"][len(response2.json()["data"])-1]
-        global mhz2
-        mhz2 = (int(data2.get(u'reportedHashrate')))/1000000
-        return mhz, mhz2
+        # response2 = requests.get("https://api.ethermine.org/miner/:0x93665d08f3581c1fa4cb30eaadee0b18ddc7b6cb/history/")
+        # data2 = response2.json()["data"][len(response2.json()["data"])-1]
+        # global mhz2
+        # mhz2 = (int(data2.get(u'reportedHashrate')))/1000000
+        # return mhz, mhz2
     except TypeError:
         print "Atention, mining trouble"
 lst=update_hasrate()
 
 print ("ReportedHashrate_bh: " + str(mhz) + " MH/s")
-print ("ReportedHashrate_bg: " + str(mhz2) + " MH/s")
-
-bot.send_message(chat_id=chat_id, text="Monitoring service has been started\n" "ReportedHashrate_bh: " + str(mhz) + " MH/s\n" "ReportedHashrate_bg: " + str(mhz2) + " MH/s")
+# print ("ReportedHashrate_bg: " + str(mhz2) + " MH/s")
+bot.send_message(chat_id=chat_id, text="Monitoring service has been started\n" "ReportedHashrate_bh: " + str(mhz) + " MH/s"
+# bot.send_message(chat_id=chat_id, text="Monitoring service has been started\n" "ReportedHashrate_bh: " + str(mhz) + " MH/s\n" "ReportedHashrate_bg: " + str(mhz2) + " MH/s")
 bot.send_message(chat_id=chat_id, text=u'\U0001F4B0' + u"\U0001F680" + u'\U0001F4B0' + u"\U0001F680" + u'\U0001F4B0' + u"\U0001F680")
 
 def monitoring():
     update_hasrate()
-    if mhz > 100 and mhz2 > 100:
+    # if mhz > 100 and mhz2 > 100:
+    if mhz > 100:
         time.sleep(600)
     else:
         bot.send_message(chat_id, "Mining has been down, needs maintenance" + u"\U0001F4A3" + u"\U0001F4A3" + u"\U0001F4A3")
