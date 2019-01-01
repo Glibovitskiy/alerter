88385375fb5fe69c17ac7a36568f265860d39875
Bohdan Glibovytskiy <Leo160886@gmail.com>
2019-01-01 15:29:49 +0200

    1_instance

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ chat_id = "-244599378"
 # bot.get_updates()[-1].message.chat_id
 
 mhz = None
-mhz2 = None
+# mhz2 = None
 def update_hasrate():
     try:
         response = requests.get("https://api.ethermine.org/miner/:0x6ddd79c6e71d4bfca125f4ae38c578af8c103daa/history/")
@@ -17,20 +17,18 @@ def update_hasrate():
         # data2 = response2.json()["data"][len(response2.json()["data"])-1]
         # global mhz2
         # mhz2 = (int(data2.get(u'reportedHashrate')))/1000000
-        # return mhz, mhz2
+        return mhz
     except TypeError:
         print "Atention, mining trouble"
 lst=update_hasrate()
 
 print ("ReportedHashrate_bh: " + str(mhz) + " MH/s")
-# print ("ReportedHashrate_bg: " + str(mhz2) + " MH/s")
-bot.send_message(chat_id=chat_id, text="Monitoring service has been started\n" "ReportedHashrate_bh: " + str(mhz) + " MH/s"
+bot.send_message(chat_id=chat_id, text="Monitoring service has been started\n" "ReportedHashrate_bh: " + str(mhz) + " MH/s")
 # bot.send_message(chat_id=chat_id, text="Monitoring service has been started\n" "ReportedHashrate_bh: " + str(mhz) + " MH/s\n" "ReportedHashrate_bg: " + str(mhz2) + " MH/s")
 bot.send_message(chat_id=chat_id, text=u'\U0001F4B0' + u"\U0001F680" + u'\U0001F4B0' + u"\U0001F680" + u'\U0001F4B0' + u"\U0001F680")
 
 def monitoring():
     update_hasrate()
-    # if mhz > 100 and mhz2 > 100:
     if mhz > 100:
         time.sleep(600)
     else:
