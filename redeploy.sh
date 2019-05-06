a4d54c4c143d7efb8e3e2456900d797b7e84898f
Bohdan <leo160886@gmail.com>
2019-05-06 14:24:30 +0300

    stable_version

--- redeploy.sh
+++
@@ -1,6 +0,0 @@
-docker pull docker.io/leo160886/alert:latest
-docker stop my-application
-docker rm my-application
-docker rmi docker.io/leo160886/alert:latest:current
-docker tag docker.io/leo160886/alert:latest docker.example.com/my-application:current
-docker run -d --name my-application docker.io/leo160886/alert:latest
